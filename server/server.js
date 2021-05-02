79f286f605a170b264cb9014b6614d7384661688
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-05-02 13:09:49 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ app.get('/',(req,res)=>{
     res.send("API is running")
 })
 
-app.use('/user',require('./routes/user'))
-app.use('/profile',require('./routes/profile'))
-app.use('/auth',require('./routes/auth'))
+app.post('/user',require('./routes/user'))
+app.get('/profile',require('./routes/profile'))
+app.get('/auth',require('./routes/auth'))
 
 app.listen(PORT,()=>{console.log('Server started on port '+PORT)})
